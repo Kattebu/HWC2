3c211f83dab409282796d1315cb7ae4cdbafbed5
Kattebu <butskoev@tut.by>
2023-02-19 01:11:08 +0300

    exercise3

---
+++ Ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Ex3
+Console.Write("введите цифру: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day==6 || day==7)
+{
+    Console.WriteLine("Да, этот день является выходным");
+}
+else if (day>=1 && day<=6)
+{
+  Console.WriteLine("Нет, день является рабочим");  
+}
+else
+{
+  Console.WriteLine("Цифра не подходит");  
+}
