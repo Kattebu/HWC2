006925acb75b203f08caddcc366fac900426c6a7
Kattebu <butskoev@tut.by>
2023-02-19 01:08:59 +0300

    exercise2

---
+++ Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Ex2
+Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >=1000000 && number<=9999999)
+{
+    Console.WriteLine("Третья цифра числа с левой стороны: " + number/10000%10);
+    Console.WriteLine("Третья цифра числа с правой стороны: " + number/100%10);
+}
+else
+{
+  Console.WriteLine("Число не семизначное");  
+}
+
