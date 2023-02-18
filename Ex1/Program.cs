20de7a8f3f2c4199ae80a92feac599317ae9cecd
Kattebu <butskoev@tut.by>
2023-02-19 01:06:38 +0300

    exercise1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Ex1
+Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >=100 && number<=999)
+{
+    Console.WriteLine("Вторая цифра числа: " + number/10%10);
+}
+else
+{
+  Console.WriteLine("Число не трехзначное");  
+}
